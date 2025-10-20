e70f28815a916d5ff99e725e5bdec3053e00ac3b
DipayanSadhukhan <dipayans0004@gmail.com>
2025-10-20 11:33:51 +0530

    edit the default link for database connection

--- Server/config/db.js
+++ Server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        const conn = await mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost:27017/sih_app');
         console.log('MongoDB connected...');
     } catch (err) {
         console.error(err.message);
